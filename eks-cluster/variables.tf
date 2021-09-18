daa1684dce39610fa682f711c965c0d9b5b0d572
julscampa <juliancampabadal@techne.local>
2021-09-18 17:33:52 -0600

    latest

--- eks-cluster/variables.tf
+++ eks-cluster/variables.tf
@@ -1,17 +1,3 @@
-variable "aws_private_key_path" {
-  description = <<DESCRIPTION
-Path to the SSH private key to be used for authentication.
-Ensure this keypair is added to your local SSH agent so provisioners can 
-connect.
-Example: ~/.ssh/cloudops.pem
-DESCRIPTION
-
-}
-
-variable "aws_key_name" {
-  description = "Name of the AWS key pair"
-}
-
 variable "region" {
   default = "us-east-2"
 }
